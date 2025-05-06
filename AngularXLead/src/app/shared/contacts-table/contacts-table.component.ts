22aae3c5d04da053fad8a3b39a8d880c9d2adbd3
Harinand S <harinand.s@experionglobal.local>
2025-05-06 15:40:26 +0530

    updated

--- AngularXLead/src/app/shared/contacts-table/contacts-table.component.ts
+++ AngularXLead/src/app/shared/contacts-table/contacts-table.component.ts
@@ -1,6 +1,8 @@
 import { AfterViewInit, Component, Input, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { MatPaginatorModule, MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { TableCheckboxComponent } from '../checkbox/checkbox.component';
 import { TableOutlineComponent } from '../table-outline/table-outline.component';
@@ -12,6 +14,8 @@ import { TableOutlineComponent } from '../table-outline/table-outline.component'
     CommonModule,
     MatTableModule,
     MatPaginatorModule,
+    MatCheckboxModule,
+    MatIconModule,
     TableCheckboxComponent,
     TableOutlineComponent
   ],
@@ -35,16 +39,21 @@ export class ContactsTableComponent implements OnInit, AfterViewInit {
   ngOnInit() {
     this.displayedColumns = ['select', ...this.headers.map(header => header.key)];
     this.dataSource.data = this.data.map(item => ({ ...item, selected: false }));
-
+    console.log('ContactsTable displayedColumns:', this.displayedColumns);
+    console.log('ContactsTable dataSource:', this.dataSource.data);
   }
 
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.paginator.length = this.dataSource.data.length;
-    this.paginator.pageSize = 5;
-    this.paginator.firstPage();
-    this.cdr.detectChanges();
-  
+    if (this.dataSource.data.length > 0) {
+      this.dataSource.paginator = this.paginator;
+      this.paginator.length = this.dataSource.data.length;
+      this.paginator.pageSize = 5;
+      this.paginator.firstPage();
+      this.cdr.detectChanges();
+      console.log('Paginator initialized:', this.paginator);
+      console.log('Paginator length:', this.paginator.length);
+      console.log('Displayed data:', this.dataSource.filteredData);
+    }
   }
 
   onPageChange(event: PageEvent) {
@@ -55,6 +64,12 @@ export class ContactsTableComponent implements OnInit, AfterViewInit {
 
   onCheckboxChange(row: any, checked: boolean) {
     row.selected = checked;
+    this.cdr.detectChanges();
+  }
+
+  onSelectAll(event: any) {
+    const checked = event.checked;
+    this.dataSource.data = this.dataSource.data.map(item => ({ ...item, selected: checked }));
     this.cdr.detectChanges();
   }
 }
--- AngularXLead/src/app/shared/table-outline/table-outline.component.ts
+++ AngularXLead/src/app/shared/table-outline/table-outline.component.ts
@@ -1,37 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatTableModule } from '@angular/material/table';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-table-outline',
   standalone: true,
-  imports: [
-    CommonModule,
-    MatTableModule,
-    MatCheckboxModule,
-    MatIconModule
-  ],
-  templateUrl: './table-outline.component.html',
-  styleUrls: ['./table-outline.component.css']
-})
-export class TableOutlineComponent implements OnInit {
-  @Input() dataSource: MatTableDataSource<any> = new MatTableDataSource<any>([]);
-  @Input() displayedColumns: string[] = [];
-  @Input() headers: { key: string; label: string }[] = [];
-
-  ngOnInit() {
-    if (!this.displayedColumns.length) {
-      this.displayedColumns = ['select', ...this.headers.map(header => header.key)];
+  imports: [CommonModule],
+  template: `
+    <div class="mat-elevation-z8 table-container">
+      <ng-content></ng-content>
+    </div>
+  `,
+  styles: [`
+    .mat-elevation-z8 {
+      box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
     }
-    console.log('TableOutline displayedColumns:', this.displayedColumns);
-    console.log('TableOutline dataSource:', this.dataSource.data);
-  }
 
-  onSelectAll(event: any) {
-    const checked = event.checked;
-    this.dataSource.data = this.dataSource.data.map(item => ({ ...item, selected: checked }));
-  }
-}
+    .table-container {
+      width: 100%;
+      border-radius: 4px;
+      overflow: hidden;
+    }
+  `]
+})
+export class TableOutlineComponent {}
